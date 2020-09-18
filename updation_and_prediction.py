466da709422a5e0b20f07bb0f2a8c60effd2fb67
Harika Reddy Chandamollu <39972138+harikachandamollu@users.noreply.github.com>
2020-09-18 22:10:29 +0200

    Add files via upload

---
+++ updation_and_prediction.py
@@ -0,0 +1,37 @@
+import numpy as np
+from forward_propagation import *
+#Function to update parameters using gradient descent
+def update_parameters(parameters, grads, learning_rate):
+    
+    L=len(parameters)//2
+    
+    for l in range(L):
+        parameters["W"+str(l+1)]=parameters["W"+str(l+1)]-learning_rate*grads["dW"+str(l+1)]
+        parameters["b"+str(l+1)]=parameters["b"+str(l+1)]-learning_rate*grads["db"+str(l+1)]
+        
+    return parameters
+
+#Function to predict using parameters
+def predict(X, y, parameters): 
+    
+    m = X.shape[1]
+    n = len(parameters) // 2 # number of layers in the neural network
+    p = np.zeros((1,m))
+    
+    # Forward propagation
+    probas, caches = model_linear_forward(X, parameters, act="relu")
+
+    
+    # convert probas to 0/1 predictions
+    for i in range(0, probas.shape[1]):
+        if probas[0,i] > 0.5:
+            p[0,i] = 1
+        else:
+            p[0,i] = 0
+    
+    #print results
+    #print ("predictions: " + str(p))
+    #print ("true labels: " + str(y))
+    print("Accuracy: "  + str(np.sum((p == y)/m)))
+        
+    return p
